d3c478a8e3a360836f143b80cd666bf434a3264b
Shota-Kurahashi <Myheroes0715@gmail.com>
2022-12-05 15:21:48 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    static: path.resolve(__dirname, "src"),
+  },
+  entry: "./src/scripts/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "./scripts/[name]-[contenthash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              //default: false
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [require("autoprefixer")({ grid: true })],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|jpeg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name]-[contenthash][ext]",
+        },
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html/,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/[name]-[chunkhash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+      filename: "templates/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
